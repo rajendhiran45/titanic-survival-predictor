dc35d51982781ba48b4b68d9a4ccc2d334d3c7f8
rajendhiran45 <rajendhiranece@gmail.com>
2026-03-13 14:20:58 +0530

    Removed unnecessary files

--- ls-2.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-data=pd.read_csv("Titanic-Dataset.csv")
-df=pd.DataFrame(data)
-print(f"It Has {df.shape[0]} Columns And {df.shape[1]} Rows")
-print(f"The Average Age Of Passengers:{df["Age"].mean()}")
-print("\nMissing Values:")
-print(df.isnull().sum())
